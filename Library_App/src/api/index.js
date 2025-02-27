6d69ca5471e0eabf35b1b5b08b9b9d57ca0d1162
laiyoung <l.d.young@outlook.com>
2025-02-26 20:29:09 -0500

    initialcommit

---
+++ Library_App/src/api/index.js
@@ -0,0 +1,86 @@
+/** API Link */
+const API_URL = `https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api`;
+
+/** Book Fetches + Updates */
+
+// Fetching All Books:
+export async function getAllBooks() {
+  try {
+    const response = await fetch(`${API_URL}/books`, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const result = await response.json();
+    return result.books;
+  } catch (error) {
+    console.error("Uh oh, trouble fetching books!", error);
+  }
+}
+
+// Single Book Fetch:
+export async function getSingleBook(id) {
+  try {
+    const response = await fetch(API_URL + `/books/${id}`);
+    const result = await response.json();
+    return result.book;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// Updating Book Availability:
+/** This ended up embedded in the Account View API Calls  */
+
+
+/** Registration + Authentification */
+
+// User Registration:
+export async function userRegistration(userData) {
+  try {
+    const response = await fetch(`${API_URL}/users/register`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userData),
+    });
+    const result = await response.json();
+    return result.token
+  } catch (err) {
+    console.error("Oops, something went wrong with your registration!", err);
+  }
+}
+
+// Login:
+export async function accountLogin(userLogin) {
+  try {
+    const response = await fetch(`${API_URL}/users/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userLogin),
+    });
+    const result = await response.json();
+    return result.token;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// Get User Info with Token:
+/** I think this function has to be inside the React component. I couldn't
+ * get it to work in the seperate API document. 
+ */
+
+
+
+/** User Account Actions */
+
+// Getting User's Book Reservations:
+/** This ended up embedded in the Account View API Calls  */
+
+// Delete Book Reservation:
+/** This ended up embedded in the Account View API Calls  */
+
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # The_Library_App
-Front end application for browsing and checking out books. 
+Front end application for browsing and checking out books. Designed as an online library for the public.
+
+Netlify Link: 
+
+Details on the API can be found 🔗 [here](https://fsa-book-buddy-b6e748d1380d.herokuapp.com/docs/)
